e53ab6384103f6ce2d268a0ada9731508fb29e51
angeles <angeless.399@gmail.com>
2024-12-01 16:41:11 -0300

    modulo codigo con js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import{header, nav, footer} from './modulos.js';
+document.querySelector('header').innerHTML = header;
+document.querySelector('nav').innerHTML = nav;
+document.querySelector('footer').innerHTML = footer;
